c5263621748a08e85e64cf2249e8a34e8ac1d345
loslimbosmike <80253568+loslimbosmike@users.noreply.github.com>
2022-12-06 19:48:18 +0100

    made fanction

--- lib/alerter/alerter.cpp
+++ lib/alerter/alerter.cpp
@@ -1,11 +1,43 @@
 #include <alerter.h>
 
 byte alertStatus;
+byte ledState = 0;
 byte ledPin;
 long currTime;
 long prvTime;
 long eventTime = 1000;
 
-void alert();
-void start();
-void alertMsg(char *msg);
+void alert()
+{
+    if (alertStatus == 1)
+    {
+        currTime = millis();
+        if (currTime - prvTime >= eventTime)
+        {
+            ledState = !ledState;
+            prvTime = currTime;
+        }
+    }
+    else if (alertStatus == 0)
+    {
+        ledState = alertStatus;
+    }
+    digitalWrite(ledPin, ledState);
+}
+void start()
+{
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(115200);
+}
+void alertMsg(char *msg)
+{
+    if (*msg == '0' || *msg == '1')
+    {
+        alertStatus = (byte)*msg;
+        alert();
+    }
+    else
+    {
+        Serial.println("No valid message");
+    }
+}
